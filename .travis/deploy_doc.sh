ba4c6fe249bd6b8043f0551dfee09a003a2611c3
Huaqi Fang <Huaqi.Fang@synopsys.com>
2018-07-09 17:33:21 +0800

    travis: only push built doc when it is non-pull-request
    Signed-off-by: Huaqi Fang <Huaqi.Fang@synopsys.com>

--- .travis/deploy_doc.sh
+++ .travis/deploy_doc.sh
@@ -19,9 +19,15 @@ ln -s ../../../example example || die
 cd ../..
 mkdir -p build/doxygen/xml || die
 make doxygen || die
-# Generate by sphinx 
+# Generate by sphinx
 make html || die
 
+# Check if this is a pull request
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    exit 0
+fi
+
+# Only commit changes when it is not a pull request
 # tar doc
 tar czvf doc.tar.gz build || die
 
@@ -44,9 +50,9 @@ git add --all || die
 # git commit -s -a -m "Update gh-pages branch, Travis build: $TRAVIS_BUILD_NUMBER, commit: ${TRAVIS_COMMIT}"
 git commit -s -a -m "Update gh-pages branch, Travis build: $TRAVIS_BUILD_NUMBER" || die
 if [ $? -eq 0 ]; then
-        git push ${REPO_LINK} gh-pages:gh-pages > /dev/null 2>&1 || die
+    git push ${REPO_LINK} gh-pages:gh-pages > /dev/null 2>&1 || die
 else
-        echo 'No update in gh-pages branch'
+    echo 'No update in gh-pages branch'
 fi
 
 exit 0
