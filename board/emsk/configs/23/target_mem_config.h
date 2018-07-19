1d908c7b612927d728c29e6d9261a3078f5a5721
Yiping Peng <yibingp@internal.synopsys.com>
2018-07-19 17:20:33 +0800

    src: new a library mcuboot
    Signed-off-by: Yiping Peng <yibingp@sysnopsys.com>

--- board/emsk/configs/23/target_mem_config.h
+++ board/emsk/configs/23/target_mem_config.h
@@ -30,6 +30,91 @@
 #ifndef _TARGET_MEM_CONFIG_H_
 #define _TARGET_MEM_CONFIG_H_
 
+#ifdef USE_MCUBOOT
+
+#ifdef USE_APPL_MEM_CONFIG
+#error "Cannot modify memory when mcuboot is enabled."
+#endif
+
+#if defined(MCUBOOT_TARGET_CONFIG)
+/*
+ * Target-specific definitions are permitted in legacy cases that
+ * don't provide the information via DTS, etc.
+ */
+#include MCUBOOT_TARGET_CONFIG
+#else
+/*
+ * Otherwise, the Zephyr SoC header and the DTS provide most
+ * everything we need.
+ */
+#ifndef FLASH_ALIGN
+#define FLASH_ALIGNMENT 8
+#define FLASH_ALIGN FLASH_ALIGNMENT
+#endif
+
+#define FLASH_DEV_NAME
+#define FLASH_AREA_IMAGE_0_OFFSET		0x10000000
+#define FLASH_AREA_IMAGE_0_SIZE			0x03000000
+#define FLASH_AREA_IMAGE_1_OFFSET		0x13000000
+#define FLASH_AREA_IMAGE_1_SIZE			0x03000000
+#define FLASH_AREA_IMAGE_SCRATCH_OFFSET		0x16000000
+#define FLASH_AREA_IMAGE_SCRATCH_SIZE		0x01000000
+#define FLASH_AREA_IMAGE_MCUBOOT_OFFSET		0x17000000
+#define FLASH_AREA_IMAGE_MCUBOOT_SIZE		0x00f00000
+
+
+#if !defined(FLASH_AREA_IMAGE_SECTOR_SIZE)
+#define FLASH_AREA_IMAGE_SECTOR_SIZE FLASH_AREA_IMAGE_SCRATCH_SIZE
+#endif
+
+#define CONFIG_FLASH_BASE_ADDRESS 0
+
+
+/*
+ * TODO: remove soc_family_kinetis.h once its flash driver supports
+ * FLASH_PAGE_LAYOUT.
+ */
+#endif /* !defined(MCUBOOT_TARGET_CONFIG) */
+
+/*
+ * Upstream Zephyr changed the name from FLASH_DRIVER_NAME to
+ * FLASH_DEV_NAME.  For now, let's just convert the Zephyr name to the
+ * one expected by MCUboot. This can be cleaned up after the upstream
+ * Zephyr tree has been released and settled down.
+ */
+#if !defined(FLASH_DRIVER_NAME) && defined(FLASH_DEV_NAME)
+#define FLASH_DRIVER_NAME FLASH_DEV_NAME
+#endif
+
+/*
+ * Sanity check the target support.
+ */
+#if !defined(FLASH_DRIVER_NAME) || \
+    !defined(FLASH_ALIGN) ||                  \
+    !defined(FLASH_AREA_IMAGE_0_OFFSET) || \
+    !defined(FLASH_AREA_IMAGE_0_SIZE) || \
+    !defined(FLASH_AREA_IMAGE_1_OFFSET) || \
+    !defined(FLASH_AREA_IMAGE_1_SIZE) || \
+    !defined(FLASH_AREA_IMAGE_SCRATCH_OFFSET) || \
+    !defined(FLASH_AREA_IMAGE_SCRATCH_SIZE)
+#warning "Target support is incomplete; cannot build mcuboot."
+#endif
+
+#ifdef MCUBOOT_COMPILE
+#define EXT_RAM_START	FLASH_AREA_IMAGE_MCUBOOT_OFFSET
+#define EXT_RAM_SIZE	FLASH_AREA_IMAGE_MCUBOOT_SIZE
+#define IMAGE_HEAD_SIZE 0x0
+#else
+#define EXT_RAM_START	FLASH_AREA_IMAGE_0_OFFSET
+#define EXT_RAM_SIZE	FLASH_AREA_IMAGE_0_SIZE
+#define IMAGE_HEAD_SIZE 0x400
+#endif
+
+#define REGION_ROM	EXT_RAM
+#define REGION_RAM	EXT_RAM
+
+#else
+
 #include "arc_feature_config.h"
 
 #ifdef USE_APPL_MEM_CONFIG
@@ -96,5 +181,9 @@
 #define REGION_RAM	DCCM
 #endif
 #endif
+
+#define IMAGE_HEAD_SIZE 0x0
+
+#endif /* !defined(USE_MCUBOOT) */
 
 #endif /* _TARGET_MEM_CONFIG_H_ */
