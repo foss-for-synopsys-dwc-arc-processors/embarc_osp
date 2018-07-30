43611c83e8a90bf8c5e1fd17a479ac5362567293
Yiping Peng <yibingp@internal.synopsys.com>
2018-07-30 13:35:17 +0800

    src: remove example secureboot

--- example/baremetal/secureboot/appl_mem_config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _APPL_MEM_CONFIG_H_
-#define _APPL_MEM_CONFIG_H_
-
-#ifdef BOARD_EMSK
-#define EXT_RAM_START	0x17F00000	/* boootloader application start address */
-#define EXT_RAM_SIZE	0x100000	/* the mem space covered by bootloader */
-#endif
-
-#endif
