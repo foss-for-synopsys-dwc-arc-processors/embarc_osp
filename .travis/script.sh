bc16ac1707916bafe9cb3b3ecf7fbcbaf51c4d55
Jingru Wang <jingru@synopsys.com>
2018-08-09 16:12:27 +0800

    travis: show the jobs in pending
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -8,6 +8,7 @@ die() {
 set -x
 
 [ $TRAVIS_OS_NAME != linux ] || {
+    bash -c "$STATUS" pending "Local $NAME testing is in progress" || die
     export PATH=/tmp/arc_gnu_${GNU_VER}_prebuilt_elf32_le_linux_install/bin:$PATH || die
     git checkout -- . || die
     bash apply_embARC_patch.sh || die
