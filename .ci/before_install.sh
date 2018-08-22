795101488f105166d2909fb1f6647e43929d6cca
Huaqi Fang <Huaqi.Fang@synopsys.com>
2018-08-22 22:07:26 +0800

    Update before_install.sh

--- .ci/before_install.sh
+++ .ci/before_install.sh
@@ -7,6 +7,10 @@ die() {
 
 if [ "${TRAVIS}" == "true" ] ; then
     [ "${TRAVIS_OS_NAME}" != "linux" ] || {
+        if [ "$STATUS" != "" ] && [ "$NAME" != "" ] ; then
+            bash -c "$STATUS" pending "Local $NAME testing is in progress" || \
+            die "Not able to post status to github, did you set EMBARC_BOT variable in travis ci setting page"
+        fi
         sudo apt-get update || die
         sudo apt-get install lib32z1 || die
         sudo apt-get install dos2unix || die
