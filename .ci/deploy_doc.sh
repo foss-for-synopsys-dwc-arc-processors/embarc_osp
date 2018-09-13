38270f2a092a38f5a2a5c39c1ec13bd466a6f443
Jingru Wang <jingru@synopsys.com>
2018-09-13 12:29:02 +0800

    travis: send comment from bash script
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .ci/deploy_doc.sh
+++ .ci/deploy_doc.sh
@@ -25,7 +25,7 @@ make html &> build_html.log || { tail -n 100 build_html.log ; die "Build sphinx 
 # Check if this is a pull request
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo "Don't push built docs to gh-pages for pull request "
-    COMMENT=$(make linkcheck || die "make linkcheck failed")
+    COMMENT=$(make linkcheck 2>&1)
     bash -c "$COMMENTS"
     exit 0
 fi
