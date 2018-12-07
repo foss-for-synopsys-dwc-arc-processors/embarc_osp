c51279693f578cad839a3ba9aa662bf3b8b664f2
Wayne Ren <wei.ren@synopsys.com>
2018-12-07 17:21:03 +0800

    board: emsdp: optimize the core_configs.mk
    *  load core_config.mk if it exists
    *  use core_hardware.h to get the name of config
    *  makefile cleanup
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- board/emsdp/rev2/configs/core_configs.mk
+++ board/emsdp/rev2/configs/core_configs.mk
@@ -1,5 +1,5 @@
 ## Current Supported Core Configurations
-CORE_DIRS = $(sort $(dir $(wildcard $(BOARD_EMSDP_DIR)/$(VALID_BD_VER)/configs/*/*.h)))
+CORE_DIRS = $(sort $(dir $(wildcard $(BOARD_EMSDP_DIR)/$(VALID_BD_VER)/configs/*/core_hardware.h)))
 SUPPORTED_CORES := $(patsubst $(BOARD_EMSDP_DIR)/$(VALID_BD_VER)/configs/%/, %,$(CORE_DIRS))
 
 CUR_CORE ?= $(firstword $(SUPPORTED_CORES))
@@ -11,7 +11,20 @@ VALID_CUR_CORE = $(call check_item_exist, $(CUR_CORE), $(SUPPORTED_CORES))
 
 BOARD_CORE_DIR = $(BOARD_EMSDP_DIR)/$(VALID_BD_VER)/configs/$(VALID_CUR_CORE)
 
-ONCHIP_IP_LIST ?= . designware/spi designware/uart designware/gpio designware/sdio designware/trng designware/pwm_timer subsystem/spi subsystem/gpio subsystem/uart subsystem/iic subsystem/i2s
+CORE_CONFIG_MK = $(BOARD_CORE_DIR)/core_config.mk
+
+##
+# load core specific makefie if exist
+##
+ifneq ($(wildcard $(CORE_CONFIG_MK)),)
+COMMON_COMPILE_PREREQUISITES += $(CORE_CONFIG_MK)
+include $(CORE_CONFIG_MK)
+endif
+
+ONCHIP_IP_LIST ?= . designware/spi designware/uart designware/gpio  \
+		designware/sdio designware/trng designware/pwm_timer \
+		subsystem/spi subsystem/gpio subsystem/uart subsystem/iic \
+		subsystem/i2s
 
 ## Check TCF file existence
 ifneq ($(TCFFILE_IS_VALID),1)
@@ -27,7 +40,7 @@ endif
 endif
 
 ## Check Core Configuration Supported
-ifeq ($(TCFFILE_IS_VALID),1)
+ifneq ($(TCFFILE_IS_VALID),1)
 ifeq ($(VALID_CUR_CORE),)
 $(info BOARD $(BOARD)-$(BD_VER) Core Configurations - $(SUPPORTED_CORES) are supported)
 $(error $(CUR_CORE) is not supported in $(BOARD)-$(BD_VER), please check it!)
