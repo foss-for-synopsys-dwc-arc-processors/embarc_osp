2d28a3e347433ba6b54aaf0e10a54a4dfdd4199c
Watson Zeng <zhiwei@synopsys.com>
2019-04-12 16:14:05 +0800

    device: flash: fl256s: add quad read function
    Signed-off-by: Watson Zeng <zhiwei@synopsys.com>

--- device/peripheral/flash/fl256s/spi_flash_fl256s.c
+++ device/peripheral/flash/fl256s/spi_flash_fl256s.c
@@ -33,14 +33,18 @@
 //#define DBG_MORE
 #include "embARC_debug.h"
 
-
-#define SF_READ_ID			0x9F
-#define SF_READ_STS			0x05
-#define SF_WRITE_ENABLE		0x06
-#define SF_WRITE_DISABLE	0x04
-#define SF_SECTOR_ERASE		0xD8
-#define SF_PAGE_WRITE		0x02 //0x32  //Quad Page Program (TODO: unsupported)
-#define SF_PAGE_READ		0x03 //0x6B  //Quad Page Read (TODO: unsupported)
+#define RDID	0x9F	/*!<read chip ID */
+#define RDSR1	0x05	/*!< read status register-1 */
+#define WRSR1	0x01	/*!< write status registe-1 */
+#define RDSR2	0x07	/*!< read status register-2 */
+#define RDCR	0x35	/*!< read config register */
+#define WREN	0x06	/*!< write enablewaitDeviceReady */
+#define WRDI	0x04	/*!< write disable */
+#define READ	0x03	/*!< read data bytes */
+#define QORD	0x6B
+#define SE		0x20	/*!< sector erase */
+#define PP		0x02	/*!< page program */
+#define QPP		0x32	/*!< page program */
 
 #define FL256S_NOT_VALID	(0xFFFFFFFF)
 
@@ -51,7 +55,7 @@ Inline int32_t _spi_send_cmd(FL256S_DEF_PTR dev, DEV_SPI_TRANSFER *xfer){
 
 	spi_flash = spi_get_dev(dev->spi_master);
 
-	cpu_status = cpu_lock_save();
+	//cpu_status = cpu_lock_save();
 
 	/* select device */
 	spi_flash->spi_control(SPI_CMD_MST_SEL_DEV, CONV2VOID((uint32_t)dev->cs));
@@ -61,28 +65,134 @@ Inline int32_t _spi_send_cmd(FL256S_DEF_PTR dev, DEV_SPI_TRANSFER *xfer){
 	/* deselect device */
 	spi_flash->spi_control(SPI_CMD_MST_DSEL_DEV, CONV2VOID((uint32_t)dev->cs));
 
-	cpu_unlock_restore(cpu_status);
+	//cpu_unlock_restore(cpu_status);
 	return ercd;
 }
 
-uint32_t fl256s_read_status(FL256S_DEF_PTR dev){
+Inline int32_t _spi_send_cmd_quad(FL256S_DEF_PTR dev, DEV_SPI_PAK_PTR pak_ptr){
+	uint32_t cpu_status;
+	DEV_SPI_PTR spi_flash;
+	int32_t ercd = 0;
 
-	uint8_t local_buf[2];
-	DEV_SPI_TRANSFER cmd_xfer;
+	spi_flash = spi_get_dev(dev->spi_master);
 
-	local_buf[0] = SF_READ_STS;
+	//cpu_status = cpu_lock_save();
 
-	DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, local_buf, 0, 1);
-	DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, local_buf, 0, 2);
-	DEV_SPI_XFER_SET_NEXT(&cmd_xfer, NULL);
+	/* select device */
+	spi_flash->spi_control(SPI_CMD_MST_SEL_DEV, CONV2VOID((uint32_t)dev->cs));
+	spi_flash->spi_control(SPI_CMD_MST_SET_FREQ, CONV2VOID(dev->spi_freq));
+	spi_flash->spi_control(SPI_CMD_SET_CLK_MODE, CONV2VOID(SPI_CLK_MODE_0));
 
-	if(_spi_send_cmd(dev, &cmd_xfer) == 0) {
-		dbg_printf(DBG_MORE_INFO, "fl256s_read_status buf: 0x%x, 0x%x\r\n", local_buf[0], local_buf[1]);
-		return local_buf[1];
+	ercd = spi_flash->spi_control(SPI_CMD_QUAD_READ, CONV2VOID(pak_ptr));
+	/* deselect device */
+	spi_flash->spi_control(SPI_CMD_MST_DSEL_DEV, CONV2VOID((uint32_t)dev->cs));
+
+	//cpu_unlock_restore(cpu_status);
+	return ercd;
+}
+
+uint32_t fl256s_read_reg(FL256S_DEF_PTR dev, uint8_t reg, uint8_t *data, uint32_t len)
+{
+
+	DEV_SPI_TRANSFER cmd_xfer = {0};
+	DEV_SPI_TRANSFER data_xfer = {0};
+
+	DEV_SPI_XFER_SET_TXBUF(&data_xfer, NULL, 0, 0);
+	DEV_SPI_XFER_SET_RXBUF(&data_xfer, data, 0, len);
+	DEV_SPI_XFER_SET_NEXT(&data_xfer, NULL);
+
+	DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, &reg, 0, 1);
+	DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, NULL, 0, 0);
+	DEV_SPI_XFER_SET_NEXT(&cmd_xfer, &data_xfer);
+
+
+	if (_spi_send_cmd(dev, &cmd_xfer) == 0) {
+		return len;
 	} else {
-		dbg_printf(DBG_MORE_INFO, "fl256s_read_status buf: 0x%x, 0x%x\r\n", local_buf[0], local_buf[1]);
 		return FL256S_NOT_VALID;
 	}
+}
+
+uint32_t fl256s_write_reg(FL256S_DEF_PTR dev, uint8_t reg, uint8_t *data, uint32_t len)
+{
+
+	DEV_SPI_TRANSFER cmd_xfer = {0};
+	DEV_SPI_TRANSFER data_xfer = {0};
+
+	DEV_SPI_XFER_SET_TXBUF(&data_xfer, data, 0, len);
+	DEV_SPI_XFER_SET_RXBUF(&data_xfer, NULL, 0, 0);
+	DEV_SPI_XFER_SET_NEXT(&data_xfer, NULL);
+
+	DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, &reg, 0, 1);
+	DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, NULL, 0, 0);
+	DEV_SPI_XFER_SET_NEXT(&cmd_xfer, &data_xfer);
+
+
+	if (_spi_send_cmd(dev, &cmd_xfer) == 0) {
+		return len;
+	} else {
+		return FL256S_NOT_VALID;
+	}
+}
+
+int32_t fl256s_quad_enable(FL256S_DEF_PTR dev)
+{
+	uint32_t status = 0;
+	uint8_t w_data[2] = {0};
+	uint8_t r_data = 0;
+	status = fl256s_read_reg(dev, RDCR, &r_data, 1);
+	if (status == FL256S_NOT_VALID) {
+		return -1;
+	}
+	if (!(r_data&0x02)) {
+		w_data[1] = r_data | 0x02;
+		status = fl256s_read_reg(dev, RDSR1, &r_data, 1);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+		w_data[0] = r_data;
+		if (fl256s_write_enable(dev) != 0) {
+			return -1;
+		}
+		status = fl256s_write_reg(dev, WRSR1, w_data, 2);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+		dev->quad_en = 1;
+	} else {
+		dev->quad_en = 1;
+	}
+	return 0;
+}
+
+int32_t fl256s_quad_disable(FL256S_DEF_PTR dev)
+{
+	uint32_t status = 0;
+	uint8_t w_data[2] = {0};
+	uint8_t r_data = 0;
+	status = fl256s_read_reg(dev, RDCR, &r_data, 1);
+	if (status == FL256S_NOT_VALID) {
+		return -1;
+	}
+	if (r_data&0x02) {
+		w_data[1] = r_data & 0xFD;
+		status = fl256s_read_reg(dev, RDSR1, &r_data, 1);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+		w_data[0] = r_data;
+		if (fl256s_write_enable(dev) != 0) {
+			return -1;
+		}
+		status = fl256s_write_reg(dev, WRSR1, w_data, 2);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+		dev->quad_en = 0;
+	} else {
+		dev->quad_en = 0;
+	}
+	return 0;
 }
 
 uint32_t fl256s_read_id(FL256S_DEF_PTR dev){
@@ -90,7 +200,7 @@ uint32_t fl256s_read_id(FL256S_DEF_PTR dev){
 	uint8_t local_buf[5];
 	DEV_SPI_TRANSFER cmd_xfer;
 
-	local_buf[0] = SF_READ_ID;
+	local_buf[0] = RDID;
 
 	DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, local_buf, 0, 1);
 	DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, local_buf, 0, 5);
@@ -108,13 +218,13 @@ uint32_t fl256s_read_id(FL256S_DEF_PTR dev){
 
 int32_t fl256s_wait_ready(FL256S_DEF_PTR dev){
 	uint32_t status = 0x01;
+	uint8_t r_data = 0;
 	do {
-		status = fl256s_read_status(dev);
-
-		if(status == FL256S_NOT_VALID) {
+		status = fl256s_read_reg(dev, RDSR1, &r_data, 1);
+		if (status == FL256S_NOT_VALID) {
 			return -1;
 		}
-	} while (status & 0x01);//Status Register 1 Bit 0
+	} while (r_data & 0x01);//Status Register 1 Bit 0
 
 	return 0;
 }
@@ -139,55 +249,54 @@ int32_t fl256s_init(FL256S_DEF_PTR dev, uint32_t freq){
 }
 
 int32_t fl256s_write_enable(FL256S_DEF_PTR dev){
-	uint8_t local_buf[3];
-	DEV_SPI_TRANSFER cmd_xfer;
-
 	uint32_t status = 0;
+	uint8_t w_data, r_data = 0;
 	do {
-		local_buf[0] = SF_WRITE_ENABLE;
-
-		DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, local_buf, 0, 1);
-		DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, NULL, 0, 0);
-		DEV_SPI_XFER_SET_NEXT(&cmd_xfer, NULL);
-
-		if (_spi_send_cmd(dev, &cmd_xfer) != 0) {
-			return -1;
-		}
-
-		status = fl256s_read_status(dev);
-
-		dbg_printf(DBG_MORE_INFO, "fl256s_write_enable status = 0x%x\r\n", status);
+		status = fl256s_write_reg(dev, WREN, &w_data, 0);
 		if (status == FL256S_NOT_VALID) {
 			return -1;
 		}
-	} while ( !(status & 0x02) );//Status Register 1 Bit 1
+
+		status = fl256s_read_reg(dev, RDSR1, &r_data, 1);
+
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+		// clear protection bits
+		//  Write Protect. and Write Enable.
+		if( (r_data & 0xfc) && (r_data & 0x02) ) {
+			w_data = 0;
+
+			status = fl256s_write_reg(dev, WRSR1, &w_data, 1);
+			if (status == FL256S_NOT_VALID) {
+				return -1;
+			}
+			r_data = 0;
+		}
+	} while ( r_data != 0x02);
 
 	return 0;
 }
 
 int32_t fl256s_write_disable(FL256S_DEF_PTR dev){
-	uint8_t local_buf[3];
-	DEV_SPI_TRANSFER cmd_xfer;
-
 	uint32_t status = 0;
+	uint8_t w_data, r_data = 0;
 	do {
-		local_buf[0] = SF_WRITE_DISABLE;
-
-		DEV_SPI_XFER_SET_TXBUF(&cmd_xfer, local_buf, 0, 1);
-		DEV_SPI_XFER_SET_RXBUF(&cmd_xfer, NULL, 0, 0);
-		DEV_SPI_XFER_SET_NEXT(&cmd_xfer, NULL);
-
-		if (_spi_send_cmd(dev, &cmd_xfer) != 0) {
-			return -1;
-		}
-
-		status = fl256s_read_status(dev);
-
-		dbg_printf(DBG_MORE_INFO, "fl256s_write_disable status = 0x%x\r\n", status);
+		status = fl256s_write_reg(dev, WRDI, &w_data, 0);
 		if (status == FL256S_NOT_VALID) {
 			return -1;
 		}
-	} while (status & 0x02);//Status Register 1 Bit 1
+
+		status = fl256s_read_reg(dev, RDSR1, &r_data, 1);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+
+		dbg_printf(DBG_MORE_INFO, "fl256s_write_disable status = 0x%x\r\n", r_data);
+		if (status == FL256S_NOT_VALID) {
+			return -1;
+		}
+	} while (r_data & 0x02);//Status Register 1 Bit 1
 
 	return 0;
 
@@ -216,7 +325,7 @@ int32_t fl256s_erase(FL256S_DEF_PTR dev, uint32_t address, uint32_t size)
 			return -1;
 		}
 
-		local_buf[0] = SF_SECTOR_ERASE;
+		local_buf[0] = SE;
 		local_buf[1] = (address >> 16) & 0xff;
 		local_buf[2] = (address >> 8) & 0xff;
 		local_buf[3] =  address & 0xff;
@@ -270,7 +379,7 @@ int32_t fl256s_write(FL256S_DEF_PTR dev, uint32_t address, uint32_t size, const 
 
 		first = first < size ? first : size;
 
-		local_buf[0] = SF_PAGE_WRITE;
+		local_buf[0] = PP;
 		local_buf[1] = (address >> 16) & 0xff;
 		local_buf[2] = (address >> 8) & 0xff;
 		local_buf[3] = address  & 0xff;
@@ -320,7 +429,7 @@ int32_t fl256s_read(FL256S_DEF_PTR dev, uint32_t address, uint32_t size, void *d
 
 		first = first < size ? first : size;
 
-		local_buf[0] = SF_PAGE_READ;
+		local_buf[0] = READ;
 		local_buf[1] = (address >> 16) & 0xff;
 		local_buf[2] = (address >> 8) & 0xff;
 		local_buf[3] = address  & 0xff;
@@ -340,6 +449,41 @@ int32_t fl256s_read(FL256S_DEF_PTR dev, uint32_t address, uint32_t size, void *d
 		address += first;
 		data += first;
 		first = FLASH_PAGE_SIZE;
+
+	} while (size);
+
+	return (int32_t)(size_orig);
+}
+
+int32_t fl256s_quad_read(FL256S_DEF_PTR dev, uint32_t address, uint32_t size, void *data)
+{
+	DEV_SPI_PAK spi_pak;
+	uint32_t first = 0;
+	uint32_t size_orig = size;
+
+	uint32_t per_read_size = 32;
+
+	first = per_read_size - (address & (per_read_size - 1));
+
+	do {
+		if (fl256s_wait_ready(dev) != 0) {
+			return -1;
+		}
+
+		first = first < size ? first : size;
+
+		spi_pak.cmd = QORD;
+		spi_pak.addr32 = address;
+		spi_pak.data_len = first;
+		spi_pak.data_ptr = data;
+
+		if (_spi_send_cmd_quad(dev, &spi_pak) != 0) {
+			return -1;
+		}
+		size -= first;
+		address += first;
+		data += first;
+		first = per_read_size;
 
 	} while (size);
 
