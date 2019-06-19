003e5940c3bd3d1252015bf35d9b2ff54665c7e0
Wayne Ren <wei.ren@synopsys.com>
2019-06-19 13:07:22 +0800

    buildsystem: only enable OS aware debug when gui is the make goal
    if make goal is run, no need to enable OS aware debug
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- options/debug.mk
+++ options/debug.mk
@@ -54,7 +54,9 @@ ifeq ($(DBG), mdb)
 ## Common options for mdb ##
 DBG_HW_FLAGS += -nooptions -nogoifmain -toggle=include_local_symbols=1
 ifeq ($(OS_SEL), freertos)
+ifeq ($(MAKECMDGOALS),gui)
 DBG_HW_FLAGS += -OS=FreeRTOS
+endif
 endif
 endif
 
