285b3c63fd4ade8ee6ad899d2daff335ccef6398
Wayne Ren <wei.ren@synopsys.com>
2019-06-19 13:04:37 +0800

    board: nsim: remove nsim_isa_host_timer options
    nsim_isa_host_timer options have side effects,
    remove it to make code running stable
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>

--- board/nsim/configs/core_compiler.mk
+++ board/nsim/configs/core_compiler.mk
@@ -16,9 +16,7 @@ ACORE_OPT_GNU   += $(COMMON_CORE_OPT_GNU)
 LCORE_OPT_GNU   += $(COMMON_CORE_OPT_GNU)
 
 ## Metaware Debugger and nSIM Related Options ##
-MDB_NSIM_OPT += -prop=nsim_isa_host_timer=1 -prop=nsim_isa_host_timer_mhz=10 \
-		@$(EMBARC_GENERATED_DIR)/$(GENE_MDB_ARG)
+MDB_NSIM_OPT += @$(EMBARC_GENERATED_DIR)/$(GENE_MDB_ARG)
 
-NSIMDRV_OPT = -p nsim_isa_host_timer=1 -p nsim_isa_host_timer_mhz=10 \
-		-propsfile $(EMBARC_GENERATED_DIR)/$(GENE_NSIM_PROPS)
+NSIMDRV_OPT = -propsfile $(EMBARC_GENERATED_DIR)/$(GENE_NSIM_PROPS)
 endif
