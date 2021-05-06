1591ac90312cd3d403928aa387d35c98dcf67e0f
Jingru Wang <jingru@synopsys.com>
2021-05-06 13:27:25 +0800

    travis: gitlab ci no longer supported
    Jenkins ci is more appropriate alterative
    
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .ci/script.sh
+++ .ci/script.sh
@@ -27,17 +27,7 @@ prepare_env() {
     : >$test_log
 
     TOOLCHAIN_CACHE_FOLDER=".cache/toolchain"
-    ARC_DEV_GNU_ROOT="/u/arcgnu_verif/gnu_builds"
-    ARC_DEV_MW_ROOT="/u/relauto/.TOOLS_ROOT/ToolsCommon/MWDT_eng/"
 
-    if [ "${TOOLCHAIN}" == "gnu" ] ; then
-        ARC_DEV_TOOL_ROOT="${ARC_DEV_GNU_ROOT}/${TOOLCHAIN_VER}/elf32_le_linux"
-    else
-        ARC_DEV_TOOL_ROOT="${ARC_DEV_MW_ROOT}/mwdt_${TOOLCHAIN_VER}/linux/ARC"
-        if [ ! -d $ARC_DEV_TOOL_ROOT ] ; then
-            ARC_DEV_TOOL_ROOT="${ARC_DEV_MW_ROOT}/${TOOLCHAIN_VER}/linux/ARC"
-        fi
-    fi
     [ "${TRAVIS}" == "true" ] && {
         if [ "${TOOLCHAIN}" == "gnu" ] ; then
             python .ci/toolchain.py -v $TOOLCHAIN_VER -c $TOOLCHAIN_CACHE_FOLDER  || die
@@ -46,30 +36,20 @@ prepare_env() {
                     ARC_DEV_TOOL_ROOT="${TOOLCHAIN_CACHE_FOLDER}/${TOOLCHAIN_VER}"
                 fi
             fi
-        elif [ "${TOOLCHAIN}" == "mw" ] ; then
-            die "Metaware toolchain is not supported in Travis CI now."
-        elif [ "${TOOLCHAIN}" == "sphinx" ] ; then
-            echo "Toolchain for building document"
         else
             die "Toolchain ${TOOLCHAIN} not supported in travis ci"
         fi
     }
 
     if [ -d $ARC_DEV_TOOL_ROOT ] ; then
-        bash .ci/linux_env_set_arc.sh -t $TOOLCHAIN -r $ARC_DEV_TOOL_ROOT || die
-        [ ! -e "arctool.env" ] && die "arctool.env doesn't exist"
-        source arctool.env || die
-        rm -rf arctool.env || die
+        REAL_ARC_DEV_ROOT=$(readlink -f ${ARC_DEV_TOOL_ROOT})
+		export ARC_GNU_ROOT=${REAL_ARC_DEV_ROOT}
+		export PATH=${REAL_ARC_DEV_ROOT}/bin:${PATH}
+        arc-elf32-gcc -v || die "ARC GNU toolchain is not installed correctly"
     else
         die "The toolchain path ${ARC_DEV_TOOL_ROOT} does not exist "
     fi
     git checkout -- . || die
-
-    if [ "${TOOLCHAIN}" == "gnu" ] ; then
-        arc-elf32-gcc -v || die "ARC GNU toolchain is not installed correctly"
-    else
-        ccac -v || die "MWDT toolchain is not installed correctly"
-    fi
 
     echo "Using ${TOOLCHAIN}-${TOOLCHAIN_VER}" || die
     . apply_embARC_patch.sh
